ecd62fe527263a73e716c43b7a9080a820e243f6
Maryam <maryamch1423@gmail.com>
2026-06-27 18:45:45 +0330

    Add comment to claculator bot module

--- calculator_bot.py
+++ calculator_bot.py
@@ -10,11 +10,13 @@ bot = telebot.TeleBot(token=token)
 
 @bot.message_handler(commands=['start'])
 def welcome_message(message):
+    """Robot introduces himself"""
     bot.send_message(chat_id=message.chat.id, 
         text="Hello👋! I'm a simple calculator.\nEnter your two numbers in this form: 10 + 5")
 
 @bot.message_handler(func=lambda msg:True)
 def calculate_numbers(message):
+    """Robot calculates the submitted numbers"""
     parts = message.text.split()
 
     try:
@@ -40,9 +42,11 @@ def calculate_numbers(message):
         bot.reply_to(message=message, text=f"✅the answer is {round(answer, 2)}")
 
     except ValueError:
+        #If user sent text or sent numbers in wrong format
         bot.reply_to(message=message, text="❌Please just send valid numbers in right format! (Format: 10 + 5) ")
 
     except ZeroDivisionError:
+        #If user wanted to divide a number by zero
         bot.reply_to(message=message, text="❌Can't division by zero! (Format: 10 + 5)")
 
 bot.infinity_polling()
